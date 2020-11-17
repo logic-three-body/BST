55095d6d9f1f20c4177c1e59a07513df5f62fbf6
logic-three-body <1552451580@qq.com>
2020-11-17 23:07:49 +0800

    AVL HEIGHT should figure out the AC version and the Book version

---
+++ AVL_HEIGHT for OJ/AVL_HEIGHT for OJ.cpp
@@ -0,0 +1,39 @@
+﻿#include<iostream>
+using namespace std;
+typedef struct BiTNode
+{
+	char data;
+	struct BiTNode *lchild, *rchild;
+}BiTNode, *BiTree;
+void CreateBiTree(BiTree &T, char S[], int &i)
+{
+	if (S[i] == '#')
+		T = nullptr;
+	else
+	{
+		T = new BiTNode;
+		T->data = S[i];
+		CreateBiTree(T->lchild, S, ++i);
+		CreateBiTree(T->rchild, S, ++i);
+	}
+}
+int Height(BiTree T)
+{
+	if (!T)
+		return 0;
+	else if (!T->lchild && !T->rchild)
+		return 1;
+	return Height(T->lchild) >= Height(T->rchild) ? Height(T->lchild) + 1 : Height(T->rchild) + 1;
+}
+int main()
+{
+	char inp[200];
+	while (cin >> inp && inp[0] != '#')
+	{
+		int i = -1;
+		BiTree T;
+		CreateBiTree(T, inp, ++i);
+		cout << Height(T) << endl;
+	}
+	return 0;
+}
